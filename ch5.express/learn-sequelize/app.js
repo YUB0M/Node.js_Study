bc560de6cb5a7f7c8bff4b516f85cf72b147a47f
YUB0M <spring-@kakao.com>
2021-06-13 16:18:43 +0900

    sequelize 알아보기

--- ch5.express/learn-sequelize/app.js
+++ ch5.express/learn-sequelize/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
 const { sequelize } = require('./models');
+//router연결
 const indexRouter = require('./routes');
 const usersRouter = require('./routes/users');
 const commentsRouter = require('./routes/comments');
--- ch5.express/learn-sequelize/routes/comments.js
+++ ch5.express/learn-sequelize/routes/comments.js
@@ -1,4 +1,5 @@
 const express = require('express');
+//const User = require("../models/user");
 const { Comment } = require('../models');
 
 const router = express.Router();
@@ -10,7 +11,17 @@ router.post('/', async (req, res, next) => {
            comment: req.body.comment,
        });
        console.log(comment);
-       res.static(201).json(comment);
+       // 201 : HTTP 성공 상태 응답 코드 요청이 성공하여 리소스를 생성했음을 나타냄
+       res.status(201).json(comment);
+
+       // //댓글 작성 후 작성자와 유저 연결
+       // const user = await User.findOne({ where : { id : req.body.id }});
+       // const comment = await Comment.create({
+       //     comment: req.body.comment,
+       // });
+       // const userComment = await user.addComment(comment);
+       // console.log(comment);
+       // res.status(201).json(userComment);
    } catch (err) {
        console.error(err);
        next(err);
